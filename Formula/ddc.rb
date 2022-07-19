2e1c5846a08105da8146e6965fbb65155dd43f03
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2022-07-19 10:22:45 +0200

    typo in class name, added README.md

--- Formula/ddc.rb
+++ Formula/ddc.rb
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-class  DDC < Formula
+class  Ddc < Formula
 
   desc "Collects logs, configuration and diagnostics from Kubernetes and On-Prem deployments of Dremio"
   homepage "https://github.com/rsvihladremio/dremio-diagnostic-collector"
---
+++ README.md
@@ -0,0 +1,12 @@
+# homebrew-ddc
+
+Provides a homebrew tap for [dremio-diagnostic-collector](https://github.com/rsvihladremio/dremio-diagnostic-collector) or ddc for short
+
+## How to use
+
+Simplicity itself
+
+```sh
+brew tap rsvihladremio/ddc
+brew install ddc
+```
